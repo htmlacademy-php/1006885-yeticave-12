ef64815c08e7cbe87fbf2b163285730c98253f48
Andrey Belyaev <ianbel263@gmail.com>
2021-02-12 15:16:41 +0300

    Устраняет замечания

--- schema.sql
+++ schema.sql
@@ -6,53 +6,53 @@ USE yeticave;
 
 CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(64) NOT NULL UNIQUE,
-  class_name VARCHAR(32) NOT NULL UNIQUE
+  category_name VARCHAR(128) NOT NULL UNIQUE,
+  code VARCHAR(64) NOT NULL UNIQUE
 );
 
-CREATE TABLE users (
+CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
   lot_id INT,
   rate_id INT,
   email VARCHAR(128) NOT NULL UNIQUE,
-  password CHAR(64) NOT NULL,
-  name VARCHAR(128) NOT NULL,
+  pwd CHAR(64) NOT NULL,
+  username VARCHAR(128) NOT NULL,
   contacts TEXT,
-  dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  date_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE lots (
+CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
   author_id INT,
   winner_id INT,
   category_id INT,
-  name VARCHAR(64) NOT NULL,
-  description VARCHAR(255),
-  img_url VARCHAR(128),
-  price DECIMAL(10,2) NOT NULL,
+  lot_name VARCHAR(255) NOT NULL,
+  lot_desc VARCHAR(255),
+  img_url VARCHAR(255),
+  lot_price DECIMAL(10,2) NOT NULL,
   rate_step TINYINT NOT NULL,
-  dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  dt_exp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (author_id) REFERENCES users(id),
-  FOREIGN KEY (winner_id) REFERENCES users(id),
+  date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  date_exp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (author_id) REFERENCES user(id),
+  FOREIGN KEY (winner_id) REFERENCES user(id),
   FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-CREATE TABLE rates (
+CREATE TABLE rate (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT,
   lot_id INT,
-  dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  price DECIMAL NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (lot_id) REFERENCES lots(id)
+  date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  rate_price DECIMAL NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(id),
+  FOREIGN KEY (lot_id) REFERENCES lot(id)
 );
 
-ALTER TABLE users ADD FOREIGN KEY (lot_id) REFERENCES lots(id);
-ALTER TABLE users ADD FOREIGN KEY (rate_id) REFERENCES rates(id);
+ALTER TABLE user ADD FOREIGN KEY (lot_id) REFERENCES lot(id);
+ALTER TABLE user ADD FOREIGN KEY (rate_id) REFERENCES rate(id);
 
-CREATE INDEX user_email ON users(email);
-CREATE INDEX lot_name ON lots(name);
-CREATE INDEX lot_price ON lots(price);
-CREATE INDEX lot_dt_add ON lots(dt_add);
-CREATE INDEX lot_dt_exp ON lots(dt_exp);
+CREATE INDEX user_email ON user(email);
+CREATE INDEX lot_name ON lot(lot_name);
+CREATE INDEX lot_price ON lot(lot_price);
+CREATE INDEX lot_dt_add ON lot(date_add);
+CREATE INDEX lot_dt_exp ON lot(date_exp);
